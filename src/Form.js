2db14b2f8e246bdb2d20aa7e35947d3f693bb153
Liliana Tuta <lilianatutza@gmail.com>
2024-12-11 22:41:32 +0000

    errors corected

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-export default function SearchEngine() {
+export default function Form() {
   const [place, setPlace] = useState("");
   const [resultMessage, setResultMessage] = useState("");
   const [busy, setBusy] = useState(false);
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import Form from "./Form";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div className="Body"> 
     <App />
-    <Form />
+      <Form />
+     
     </div>
+
   </React.StrictMode>
 );
 
